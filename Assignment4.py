d3980c4848fe1df64bb20ada42bbd7f2ca843280
Ankush Bhateja <128409237+ankushbhateja@users.noreply.github.com>
2025-05-22 02:03:00 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,50 @@
+# Task 1: Read a File and Handle Errors 
+# Problem Statement:  Write a Python program that:
+# 1.   Opens and reads a text file named sample.txt.
+# 2.   Prints its content line by line.
+# 3.   Handles errors gracefully if the file does not exist.
+
+
+# def openFile():
+#     try:
+#         with open('sample.txt', 'w') as sampleTextFile:
+#             sampleTextFile.write('This is the sample text file\nIt contains multiple lines')
+    
+#         with open('sample.txt', 'r+') as sampleTextFile:
+#             print(sampleTextFile.read())    
+            
+#     except FileNotFoundError:
+#         print("The file 'sample.txt' was not found")
+
+
+# openFile()
+
+
+# Task 2: Write and Append Data to a File
+ 
+# Problem Statement: Write a Python program that:
+# 1.   Takes user input and writes it to a file named output.txt.
+# 2.   Appends additional data to the same file.
+# 3.   Reads and displays the final content of the file.
+
+
+enteredData = input("Enter text to write to the file : ")
+
+def writeData():
+    try:
+        with open("output.txt",'w') as outputTextFile:
+            outputTextFile.write(enteredData + '\n')
+            print("Data successfully written to output.txt")
+        appendData= input("Enter additional to add in output.txt : ")
+        with open("output.txt",'a') as outputTextFile:
+            outputTextFile.write(appendData)
+        print("Data successfully appended")
+        
+        print("Final content of output.txt")
+        with open("output.txt",'r') as outputTextFile:
+           print(outputTextFile.read())
+    except FileNotFoundError:
+        print("output.txt not found")
+
+
+writeData()
